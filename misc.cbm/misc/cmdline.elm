8b8d9ca443543dae35a8a7e973c9103c344fcef9
hexdump <hexdump0815@googlemail.com>
2021-03-07 10:27:50 +0100

    v5.10.20 cbm working on krane w usb and xorg
    - fix mtk usb patch
    - removed CONFIG_DRM_VGEM
    - add sample xorg.conf for krane
    - use relative root part in cmdline (PARTUUID=%U/PARTNROFF=3)
    - add rk3399-gru bob and kevin dtbs - seems to work on those too

--- misc.cbm/misc/cmdline.elm
+++ misc.cbm/misc/cmdline.elm
@@ -1,1 +1,1 @@
-console=tty1 root=/dev/mmcblk1p4 rootfstype=ext4 rootwait ro fsck.fix=yes fsck.repair=yes net.ifnames=0 ipv6.disable=1
+console=tty1 root=PARTUUID=%U/PARTNROFF=3 rootfstype=ext4 rootwait ro fsck.fix=yes fsck.repair=yes net.ifnames=0 ipv6.disable=1
